75a6615ca53e8b6a76260712db72048c23d05216
Douglas Oliveira <douglaspo_97@outlook.com>
2025-05-13 16:22:58 -0300

    feat: create footer section

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="max-w-7xl mx-auto w-full flex items-center justify-between
+      py-4 border-t-2 border-gray-200 fixed bottom-0 left-1/2 transform -translate-x-1/2">
+      <p>Montik Product Page © 2025 - Todos os direitos reservados</p>
+      <h5>
+        Desenvolvido por {""}
+        <a href="https://www.linkedin.com/in/douglaspo/">Douglas Oliveira</a>
+      </h5>
+    </footer>
+  )
+}
